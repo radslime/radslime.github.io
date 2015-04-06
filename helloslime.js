752936a09ab1d86704050b88370f7139138852ef
radslime <maldestrous@gmail.com>
2015-04-06 00:42:32 -0400

    Create helloslime.js

---
+++ helloslime.js
@@ -0,0 +1,10 @@
+var hi = new Array ();
+hi[0] = "i make trash";
+hi[1] = "i worship satan";
+hi[2] = "i like to rhyme";
+hi[3] = "ravioli is my passion";
+hi[4] = "jet fuel can't melt steel beams";
+hi[5] = "i sleep with numerous stuffed animals";
+var i = Math.floor(6*Math.random())
+//this number ^ must always be one more than however many nameIdeas there are!!!
+document.write(hi[i]);
