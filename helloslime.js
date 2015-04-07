e4e221d35b9ec078558adef0f2003f243e5e7838
radslime <maldestrous@gmail.com>
2015-04-07 19:48:01 -0400

    Update helloslime.js

--- helloslime.js
+++ helloslime.js
@@ -7,7 +7,7 @@ hi[4] = "jet fuel can't melt steel beams";
 hi[5] = "i sleep with numerous stuffed animals";
 hi[6] = "i study electrical engineering";
 hi[7] = "this is my swamp";
-hi[8] = "you should email me: slime@radsli.me";
+hi[8] = "you can't see me";
 var i = Math.floor(9*Math.random())
 //this number ^ must always be one more than however many greetings there are!!!
 document.write(hi[i]);
